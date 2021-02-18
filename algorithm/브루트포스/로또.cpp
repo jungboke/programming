b997e41f9e7ef3549668f872a01b3a7c2c007274
jungboke <78137720+jungboke@users.noreply.github.com>
2021-02-18 16:18:21 +0900

    Update 로또.cpp

--- algorithm/브루트포스/로또.cpp
+++ algorithm/브루트포스/로또.cpp
@@ -1,9 +1,10 @@
+//모범답안
 #include <iostream>
 #include <vector>
 using namespace std;
 vector<int> lotto;
 void solve(vector<int> &a, int index, int cnt) {
-if (cnt == 6) {
+if (cnt == 6) { // 완료조건 : 6개를 선택했을때
 for (int num : lotto) {
 printf("%d ",num);
 }
@@ -11,22 +12,22 @@ printf("\n");
 return;
 }
 int n = a.size();
-if (n == index) return;
-lotto.push_back(a[index]);
-solve(a,index+1,cnt+1);
-lotto.pop_back();
-solve(a,index+1,cnt);
+if (n == index) return; // 불가능조건 : index초과
+lotto.push_back(a[index]); // 선택된 로또번호를 lotto배열에 입력
+solve(a,index+1,cnt+1); // 선택한 경우
+lotto.pop_back(); // 선택됐던 로또번호를 lotto배열에서 제거
+solve(a,index+1,cnt); // 선택하지 않은 경우
 }
 int main() {
 while (true) {
 int n;
-cin >> n;
+cin >> n; // 로또숫자 개수 입력
 if (n == 0) break;
-vector<int> a(n);
+vector<int> a(n); // 로또숫자 개수만큼의 배열
 for (int i=0; i<n; i++) {
 cin >> a[i];
 }
-solve(a, 0, 0);
+solve(a, 0, 0); // 재귀함수 호출
 cout << '\n';
 }
 return 0;
