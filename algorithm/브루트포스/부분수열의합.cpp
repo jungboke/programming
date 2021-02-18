d44b534d464d34a0c0e9b16bf56352f845c252c0
jungboke <78137720+jungboke@users.noreply.github.com>
2021-02-18 16:48:33 +0900

    Update 부분수열의합.cpp

--- algorithm/브루트포스/부분수열의합.cpp
+++ algorithm/브루트포스/부분수열의합.cpp
@@ -4,22 +4,22 @@ int a[20];
 int n, m;
 int ans = 0;
 void go(int i, int sum) {
-if (i == n) {
+if (i == n) { // 완료조건 : 마지막 index
 if (sum == m) {
 ans += 1;
 }
 return;
 }
-go(i+1, sum+a[i]);
-go(i+1, sum);
+go(i+1, sum+a[i]); // 해당index의 값 선택했을때
+go(i+1, sum); // 해당index의 값 선택하지 않았을때
 }
 int main() {
 cin >> n >> m;
 for (int i=0; i<n; i++) {
 cin >> a[i];
 }
-go(0, 0);
-if (m == 0) ans -= 1;
+go(0, 0); // 재귀함수
+if (m == 0) ans -= 1; // 부분수열의 합이 0이될때만 0개의 부분수열이 나올수 있음
 cout << ans << '\n';
 return 0;
 }
