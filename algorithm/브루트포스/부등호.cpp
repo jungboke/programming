1d38255d237b1e712f09a73fc328517279cc3f7b
jungboke <78137720+jungboke@users.noreply.github.com>
2021-02-18 13:51:44 +0900

    Update 부등호.cpp

--- algorithm/브루트포스/부등호.cpp
+++ algorithm/브루트포스/부등호.cpp
@@ -1,3 +1,4 @@
+//모범답안
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -16,17 +17,17 @@ return true;
 int main() {
 int k;
 cin >> k;
-vector<char> a(k);
+vector<char> a(k); // 부등호를 담기위한 배열 선언
 for (int i=0; i<k; i++) {
-cin >> a[i];
+cin >> a[i]; // k개의 부등호 입력
 }
-vector<int> small(k+1);
-vector<int> big(k+1);
+vector<int> small(k+1); // 최소값을 담기위한 배열 선언
+vector<int> big(k+1); // 최대값을 담기위한 배열 선언
 for (int i=0; i<=k; i++) {
 small[i] = i;
 big[i] = 9-i;
 }
-do {
+do { // 최소값을 담은 배열의 순열들을 구하기 위한 dowhile문
 if (check(small, a)) {
 break;
 }
@@ -37,11 +38,11 @@ break;
 }
 } while(prev_permutation(big.begin(), big.end()));
 for (int i=0; i<big.size(); i++) {
-cout << big[i];
+cout << big[i]; // 최대값 배열 출력
 }
 cout << '\n';
 for (int i=0; i<small.size(); i++) {
-cout << small[i];
+cout << small[i]; // 최소값 배열 
 }
 cout << '\n';
 return 0;
