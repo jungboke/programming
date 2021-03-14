4bdc0f615316f5fcce9a81433ba8909ac5a34d9f
jungboke <78137720+jungboke@users.noreply.github.com>
2021-03-14 23:26:20 +0900

    Update 레이저통신.cpp

--- algorithm/브루트포스/BFS/레이저통신.cpp
+++ algorithm/브루트포스/BFS/레이저통신.cpp
@@ -11,7 +11,7 @@ int main() {
 int m, n;
 cin >> m >> n;
 vector<string> a(n);
-int sx,sy,ex,ey;
+int sx,sy,ex,ey; // 시작점과 끝점
 sx=sy=ex=ey=-1;
 for (int i=0; i<n; i++) {
 cin >> a[i];
@@ -27,9 +27,9 @@ ey = j;
 }
 }
 }
-vector<vector<int>> d(n, vector<int>(m, -1));
+vector<vector<int>> d(n, vector<int>(m, -1)); // ** n행m열의 배열을 설정하고 -1로 초기화
 queue<pair<int,int>> q;
-d[sx][sy] = 0;
+d[sx][sy] = 0; // 해당칸까지의 직선의 개수
 q.push(make_pair(sx,sy));
 while (!q.empty()) {
 int x, y;
@@ -37,7 +37,7 @@ tie(x,y) = q.front(); q.pop();
 for (int k=0; k<4; k++) {
 int nx = x+dx[k];
 int ny = y+dy[k];
-while (0 <= nx && nx < n && 0 <= ny && ny < m) {
+while (0 <= nx && nx < n && 0 <= ny && ny < m) { // 십자형으로 퍼져나가는게 아닌 일자형으로 벽까지 퍼져나감
 if (a[nx][ny] == '*') break;
 if (d[nx][ny] == -1) {
 d[nx][ny] = d[x][y] + 1;
@@ -48,6 +48,6 @@ ny += dy[k];
 }
 }
 }
-cout << d[ex][ey]-1 << '\n';
+cout << d[ex][ey]-1 << '\n'; // 거울의 수는 직선의 수 -1 임
 return 0;
 }
