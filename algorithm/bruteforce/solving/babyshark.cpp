838cb8eb9639accafbd54314d21458cdbac96375
jungboke <tmdqjaqhdl3@naver.com>
2021-03-17 19:08:51 +0900

    new

--- algorithm/bruteforce/solving/babyshark.cpp
+++ algorithm/bruteforce/solving/babyshark.cpp
@@ -8,7 +8,16 @@ using namespace std;
 
 int main() {
 
-    
+
+    int d = 100;
+    int a = 2;
+
+    int b = 2;
+
+    int c = a + b;
+
+
+    cout << a << " + " << b << " = " << c << endl;
 
     return 0;
 }
